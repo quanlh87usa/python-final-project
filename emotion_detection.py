75d8e18002c124ce95e9dfa97ccfef5ca53adc4a
quanlh87usa <quanlh87usa@gmail.com>
2026-05-17 01:24:43 -0400

    format response

--- emotion_detection.py
+++ emotion_detection.py
@@ -10,5 +10,31 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze }}
     header =  {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header, timeout=10)
-
-    return response.text
+    if response.status_code == 200:
+        # Parse the response from the API
+        formatted_response = json.loads(response.text)
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
+        anger = emotions['anger']
+        disgust = emotions['disgust']
+        fear = emotions['fear']
+        joy = emotions['joy']
+        sadness = emotions['sadness']
+        dominant_emotion = max(emotions.values())
+    # If the response status code is 500, set label and score to None
+    elif response.status_code == 500:
+        anger = None
+        disgust = None
+        fear = None
+        joy = None
+        sadness = None
+        dominant_emotion = None
+    # For any other unexpected status codes, set label and score to None
+    else:
+        anger = None
+        disgust = None
+        fear = None
+        joy = None
+        sadness = None
+        dominant_emotion = None
+    return {'anger': anger, 'disgust': disgust, 'fear': fear, 'joy': joy
+            , 'sadness': sadness, 'dominant_emotion': dominant_emotion}
